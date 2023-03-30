c7fb4fca53b7d073740eae19f428f978dfa94fe3
Bruno Massa <brmassa@gmail.com>
2023-03-30 01:17:04 -0300

    feat: artifact from job 1 reused in the docker build job

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace ReadFileConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "asdf.txt";
+
+            try
+            {
+                string fileContent = File.ReadAllText(fileName);
+                Console.WriteLine(fileContent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
